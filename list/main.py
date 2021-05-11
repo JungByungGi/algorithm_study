9e6b19f157db59554c85473952af70e7caa29f7c
JungByungGi <qudrlwjd12@naver.com>
2021-05-11 23:40:34 +0900

    자료구조 리스트 학습

---
+++ list/main.py
@@ -0,0 +1,16 @@
+# 크기 지정을 하지 않고 배열에 값을 추가할 수 있다.
+country = 'US'
+print(country)
+
+country = country + 'A'
+print(country)
+
+# 1차원 배열 : 리스트로 구현 시
+data = [1,2,3,4,5]
+print(data)
+
+# 2차원 배열 : 리스트로 구현 시
+data = [[1,2,3], [4,5,6], [7,8,9]]
+print(data)
+print(data[0]) # 인덱스를 통한 접근
+print(data[0][0]) # 2차원 인덱스를 통한 접근
